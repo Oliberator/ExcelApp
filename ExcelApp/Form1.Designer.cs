b97272631bde6055663f3b9cfaa94bcec45c681f
Порядочный Никита <sinilschikov2@gmail.com>
2020-05-18 21:05:26 +0300

    Добавьте файлы проекта.

---
+++ ExcelApp/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ExcelApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dtGrid = new System.Windows.Forms.DataGridView();
+            this.openFile = new System.Windows.Forms.Button();
+            this.winners = new System.Windows.Forms.Button();
+            this.safeFile = new System.Windows.Forms.Button();
+            this.randTime = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtGrid
+            // 
+            this.dtGrid.BackgroundColor = System.Drawing.Color.DarkGray;
+            this.dtGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGrid.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dtGrid.Location = new System.Drawing.Point(13, 12);
+            this.dtGrid.Name = "dtGrid";
+            this.dtGrid.Size = new System.Drawing.Size(760, 411);
+            this.dtGrid.TabIndex = 4;
+            // 
+            // openFile
+            // 
+            this.openFile.BackgroundImage = global::ExcelApp.Properties.Resources.openFile;
+            this.openFile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.openFile.FlatAppearance.BorderColor = System.Drawing.Color.Aqua;
+            this.openFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.openFile.Location = new System.Drawing.Point(13, 429);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(380, 60);
+            this.openFile.TabIndex = 5;
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // winners
+            // 
+            this.winners.BackgroundImage = global::ExcelApp.Properties.Resources.winners;
+            this.winners.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.winners.FlatAppearance.BorderColor = System.Drawing.Color.Aqua;
+            this.winners.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.winners.Location = new System.Drawing.Point(393, 489);
+            this.winners.Name = "winners";
+            this.winners.Size = new System.Drawing.Size(380, 60);
+            this.winners.TabIndex = 6;
+            this.winners.UseVisualStyleBackColor = true;
+            this.winners.Click += new System.EventHandler(this.winners_Click);
+            // 
+            // safeFile
+            // 
+            this.safeFile.BackgroundImage = global::ExcelApp.Properties.Resources.safeFile;
+            this.safeFile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.safeFile.FlatAppearance.BorderColor = System.Drawing.Color.Aqua;
+            this.safeFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.safeFile.Location = new System.Drawing.Point(13, 489);
+            this.safeFile.Name = "safeFile";
+            this.safeFile.Size = new System.Drawing.Size(380, 60);
+            this.safeFile.TabIndex = 7;
+            this.safeFile.UseVisualStyleBackColor = true;
+            this.safeFile.Click += new System.EventHandler(this.safeFile_Click);
+            // 
+            // randTime
+            // 
+            this.randTime.BackgroundImage = global::ExcelApp.Properties.Resources.rand;
+            this.randTime.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.randTime.FlatAppearance.BorderColor = System.Drawing.Color.Aqua;
+            this.randTime.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.randTime.Location = new System.Drawing.Point(393, 429);
+            this.randTime.Name = "randTime";
+            this.randTime.Size = new System.Drawing.Size(380, 60);
+            this.randTime.TabIndex = 8;
+            this.randTime.UseVisualStyleBackColor = true;
+            this.randTime.Click += new System.EventHandler(this.randTime_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.randTime);
+            this.Controls.Add(this.safeFile);
+            this.Controls.Add(this.winners);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.dtGrid);
+            this.Name = "Form1";
+            this.Text = "ExcelApp";
+            ((System.ComponentModel.ISupportInitialize)(this.dtGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dtGrid;
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.Button winners;
+        private System.Windows.Forms.Button safeFile;
+        private System.Windows.Forms.Button randTime;
+    }
+}
+
---
+++ ExcelApp/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+
+namespace ExcelApp
+{
+    public partial class Form1 : Form
+    {
+        public class Winners
+        {
+            public string Name { get; set; }
+            public int Number { get; set; }
+            public override string ToString()
+            {
+                return "Участник: " + Name + "   Баллов: " + Number;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable dt = new DataTable();
+        DataTable sortedDT = new DataTable();
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            // Фильтр от лишних форматов, только xlsx.
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Excel workbook (.xlsx)|*.xlsx";
+            ofd.Title = "Выберите таблицу Excel";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // Сохранение пути выбранного файла в filePath.
+                // Создание новой книги Excel, внутри нее лист с содержимым из filePath.
+                string filePath = ofd.FileName;
+                var workbook = new XLWorkbook(filePath);
+                var worksheet = workbook.Worksheet(1);
+
+                // Если столбец есть в книге, добавляем в таблицу программы
+                // Иначе добавляем остальные строки
+                bool firstRow = true;
+                foreach (IXLRow row in worksheet.Rows())
+                {
+                    if (firstRow)
+                    {
+                        foreach (IXLCell cell in row.Cells())
+                        {
+                            dt.Columns.Add(cell.Value.ToString());
+                        }
+                        firstRow = false;
+                    }
+                    else
+                    {
+                        dt.Rows.Add();
+                        int i = 0;
+                        foreach (IXLCell cell in row.Cells())
+                        {
+                            dt.Rows[dt.Rows.Count - 1][i] = cell.Value.ToString();
+                            i++;
+                        }
+                    }
+                }
+            }
+            // ЗНачение таблицы dt в dtGrid
+            dtGrid.DataSource = dt;
+        }
+
+        private void safeFile_Click(object sender, EventArgs e)
+        {
+            // Фильтр от лишних форматов, только xlsx.
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel workbook (.xlsx)|*.xlsx";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                // Создание новой XLSX-книги, созданение листа Участники со значением таблицы sortedDT
+                // Сохранение книги в выбранное место на ПК
+                XLWorkbook wb = new XLWorkbook();
+                wb.Worksheets.Add(sortedDT, "Участники");
+                string filePath = sfd.FileName;
+                wb.SaveAs(filePath);
+            }
+            MessageBox.Show("Изменения сохранены!");
+        }
+
+        private void randTime_Click(object sender, EventArgs e)
+        {
+
+            if (dtGrid.DataSource != null)
+            {
+                if (!dt.Columns.Contains("Номер сдачи"))
+                {
+                    dt.Columns.Add("Номер сдачи", typeof(Int32));
+                    dt.Columns.Add("1 тур", typeof(Int32));
+                    dt.Columns.Add("2 тур", typeof(Int32));
+                    dt.Columns.Add("3 тур", typeof(Int32));
+                    dt.Columns.Add("Баллы", typeof(Int32));
+                }
+                foreach (DataColumn col in dt.Columns)
+                    col.ReadOnly = false;
+                Random rnd = new Random();
+                foreach (DataRow row in dt.Rows)
+                    row["Номер сдачи"] = rnd.Next(0, dt.Rows.Count);
+
+                DataView dv = dt.DefaultView;
+                dv.Sort = "Номер сдачи";
+                sortedDT = dv.ToTable();
+
+                int a = 1;
+                dtGrid.DataSource = sortedDT;
+
+                for (int i = 0; i < dtGrid.Rows.Count; i++)
+                {
+                    for (int j = 1; j < dtGrid.Rows[i].Cells.Count - 4; j++)
+                    {
+
+                        dtGrid.Rows[i].Cells[j].Value = a;
+                        a++;
+                    }
+                }
+            }
+        }
+
+        private void winners_Click(object sender, EventArgs e)
+        {
+            // Хранятся сумма всех баллов и 3-х максимальных
+            List<Winners> wins = new List<Winners>();
+
+            // Если пустой грид, то ждём пока станет не пустым
+            if (!dtGrid.Rows.Equals(""))
+            {
+                // Перебор всех строк грида
+                for (int i = 0; i < dtGrid.Rows.Count; i++)
+                {
+                    
+                    // Счётчик суммы
+                    int sum = 0;
+                    string name = "";
+                    // Перебор ячеек 1-3 тур
+                    for (int j = 2; j < dtGrid.Rows[i].Cells.Count - 1; j++)
+                    {
+                        name = Convert.ToString(dtGrid.Rows[i].Cells[0].Value);
+                        // роверка данных в ячейках
+                        int intValue;
+                        bool success = Int32.TryParse(Convert.ToString(dtGrid.Rows[i].Cells[j].Value), out intValue);
+                        // сли всё ок, считаем и добавляем сумму в ячейку Баллы
+                        if (success)
+                        {
+                            int row = Convert.ToInt32(dtGrid.Rows[i].Cells[j].Value);
+                            sum = sum + row;
+                            dtGrid.Rows[i].Cells[dtGrid.Rows[i].Cells.Count - 1].Value = sum;
+                        }
+                    }
+                    // Добавление фио и баллов в коллекцию
+                    wins.Add(new Winners() { Name = name, Number = sum });
+                }
+
+                // Вывод 3-х победителей олимпиады
+                foreach(var num in wins.OrderByDescending(n => n.Number).Take(3))
+                {
+                    MessageBox.Show(num.ToString());
+                }
+            }
+        }
+    }
+}
